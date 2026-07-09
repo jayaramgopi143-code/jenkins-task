061b138d701b55dcd06be596d0d3c00529d54b58
Jayaramgopi143-code <jayaramgopi143@gmail.com>
2026-07-09 11:22:03 +0000

    added user data for copying index.html

--- main.tf
+++ main.tf
@@ -12,4 +12,13 @@ provider "aws" {
 resource "aws_instance" "MyWk20Project" {
   ami = "ami-01a00762f46d584a1"
   instance_type = "t3.micro"
+  # Automatically installs Apache and copies your HTML on boot
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "${file("index.html")}" > /var/www/html/index.html
+              EOF
 }
