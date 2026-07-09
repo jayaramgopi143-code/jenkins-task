a20366d92839496528be0c01c1add1d4bfea798c
Jayaramgopi143-code <jayaramgopi143@gmail.com>
2026-07-09 10:07:59 +0000

    Updated Terraform configuration

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ provider "aws" {
   region = "ap-south-1"
   }
 resource "aws_instance" "MyWk20Project" {
-  ami = "ami-0e670eb768a5fc3d4"
-  instance_type = "t2.micro"
+  ami = "ami-01a00762f46d584a1"
+  instance_type = "t3.micro"
 }
